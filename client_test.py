3e2c7a96096f330f4d30598b456fe7dca3132a58
JCrossover-14 <110313504+JCrossover-14@users.noreply.github.com>
2024-07-02 00:07:01 -0400

    task 1 completed

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint,getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,7 +8,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    for quote in quotes:
+       self.assertEqual(getDataPoint(quote),(quote['stock'],quote['top_bid']['price'],quote['top_ask']['price'],(quote['top_bid']['price']+quote['top_ask']['price'])/2))
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
@@ -16,10 +17,51 @@ class ClientTest(unittest.TestCase):
     ]
     """ ------------ Add the assertion below ------------ """
 
-
+    for quote in quotes:
+       self.assertEqual(getDataPoint(quote),(quote['stock'],quote['top_bid']['price'],quote['top_ask']['price'],(quote['top_bid']['price']+quote['top_ask']['price'])/2))
   """ ------------ Add more unit tests ------------ """
 
+  def test_getRatio(self):
+     quotes = [
+      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+     price_abc = -1
+     price_def = -1
+     
+     for quote in quotes:
+        if quote['stock']=="ABC":
+           price_abc = quote['top_ask']['price']+quote['top_bid']['price']
+           price_abc = price_abc/2
+        elif quote['stock']=='DEF':
+           price_def = quote['top_ask']['price']+quote['top_bid']['price']
+           price_def = price_def/2
+     actual = 1e9
 
+     if price_def!=0:
+        actual = price_abc/price_def 
+     self.assertEqual(getRatio(price_abc,price_def),actual)
+
+  def test_getRatio_price_def_zero(self):
+     quotes = [
+      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 0, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 0, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+     price_abc = -1
+     price_def = -1
+     
+     for quote in quotes:
+        if quote['stock']=="ABC":
+           price_abc = quote['top_ask']['price']+quote['top_bid']['price']
+           price_abc = price_abc/2
+        elif quote['stock']=='DEF':
+           price_def = quote['top_ask']['price']+quote['top_bid']['price']
+           price_def = price_def/2
+     actual = 1e9
+
+     if price_def!=0:
+        actual = price_abc/price_def 
+     self.assertEqual(getRatio(price_abc,price_def),actual)
 
 if __name__ == '__main__':
     unittest.main()
